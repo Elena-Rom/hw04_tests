ead1548995f66907d1f28a4a16458be3981a7b04
Елена Ромашкова <elen.iva89@gmail.com>
2022-11-14 15:33:37 +0300

    change import

--- yatube/posts/tests/test_views.py
+++ yatube/posts/tests/test_views.py
@@ -3,7 +3,7 @@ from django.contrib.auth import get_user_model
 from django.test import Client, TestCase
 from django.urls import reverse
 
-from yatube.posts.models import Post, Group
+from ..models import Post, Group
 
 
 User = get_user_model()
