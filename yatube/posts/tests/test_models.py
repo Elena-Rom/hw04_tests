ca492f4d9f4e0af7255067dec181421a47a29182
Елена Ромашкова <elen.iva89@gmail.com>
2022-11-16 22:22:42 +0300

    fix homework2.2

--- yatube/posts/tests/test_models.py
+++ yatube/posts/tests/test_models.py
@@ -1,7 +1,8 @@
 from django.contrib.auth import get_user_model
 from django.test import TestCase
-
+from django.conf import settings
 from ..models import Group, Post
+
 
 User = get_user_model()
 
@@ -18,12 +19,15 @@ class PostModelTest(TestCase):
         )
         cls.post = Post.objects.create(
             author=cls.user,
-            text='Духовной жаждою томим,В пустыне мрачной я влачился',
+            text='Духовной жаждою',
         )
 
-    def test_models_have_correct_object_names(self):
-        """Проверяем, что у моделей корректно работает __str__."""
-        title_group = PostModelTest.group
+    def test_models_post_have_correct_object_names(self):
+        """Проверяем, что у модели Post корректно работает __str__."""
         text_post = PostModelTest.post
-        self.assertEqual(str(title_group), title_group.title)
         self.assertEqual(str(text_post), text_post.text[:15])
+
+    def test_models_group_have_correct_object_names(self):
+        """Проверяем, что у модели Group корректно работает __str__."""
+        title_group = PostModelTest.group
+        self.assertEqual(str(title_group), title_group.title)
